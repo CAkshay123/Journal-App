46e2a99a6ce31940bdc9e4006e485b970ba2de18
Akshay <akshayakshayc7@gmail.com>
2025-01-12 20:04:06 +0530

    FirstCommit

---
+++ src/main/java/com/akshayisgod/Journalling/JournallingAppApplication.java
@@ -0,0 +1,13 @@
+package com.akshayisgod.Journalling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournallingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournallingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshayisgod/Journalling/JournallingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshayisgod.Journalling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournallingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
